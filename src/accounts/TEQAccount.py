e810014b837ea13ee1dc2600ce370c257fad71b2
Veracities <philip200156@hotmail.com>
2018-10-23 22:00:39 -0400

    Modified account,TEQaccount and agencyAccount by removing code that are not needed

--- src/accounts/TEQAccount.py
+++ src/accounts/TEQAccount.py
@@ -2,23 +2,6 @@ from accounts import account
 from agencyAccount import agencyAccount
 
 class TEQ_Account(account):
-    def __init__(self, name, numID, user_name, password, level):
-        account.__init__(self,name, numID, user_name, password)
+    def __init__(self, name, numID, level):
+        account.__init__(self,name, numID)
         self.level = level # user level (low, medium, high)
-    
-    def upload_Data(self, file, database):
-        return None
-    
-    def generate_Report(self):
-        return None
-    
-    def add_data_templates(self, new_template):
-        return None
-    
-    def create_TEQ_account(self, name, numID, user_name, password, level):
-        if (self.logged_in):
-            return TEQ_Account(name, numID, user_name, password, level)
-    
-    def create_agency_account(self,agency_name, idNum, user_name, password):
-        if (self.logged_in):
-            return agencyAccount(agency_name, idNum, user_name, password)
--- src/accounts/accounts.py
+++ src/accounts/accounts.py
@@ -2,14 +2,3 @@ class account:
     def __init__(self, name, numID, user_name, password):
         self.name = name
         self.numID = numID
-        # for login
-        self.user_name = user_name
-        self.password = password        
-        self.logged_in = False # login status
-    
-    def login(self, user_name, password):
-        if (self.user_name == user_name and self.password == password):
-            self.logged_in = True
-    
-    def log_out(self):
-        self.logged_in = False
