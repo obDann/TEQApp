d8554536d5b0d4a3cafe1f74b0b65b17a44f3376
susanwang98 <wang.susan98@gmail.com>
2018-10-28 18:55:28 -0400

    Revised mysql database files to run sqlite instead and added files to run a GUI through main.py

---
+++ src/main/create_user_db.py
@@ -0,0 +1,42 @@
+import sqlite3
+
+def create_tables():
+    conn = sqlite3.connect('users.db')
+    cur = conn.cursor()
+    
+    cur.execute("PRAGMA foreign_keys = ON")
+    
+    cur.execute("""CREATE TABLE IF NOT EXISTS User (
+            Username text PRIMARY KEY NOT NULL,
+            Name text NOT NULL,
+            Password text NOT NULL,
+            Type text NOT NULL,
+            Date_Created INT NOT NULL);""")
+    
+    cur.execute("""CREATE TABLE IF NOT EXISTS Agency_User (
+            Username text PRIMARY KEY NOT NULL,
+            Status text NOT NULL,
+            FOREIGN KEY (Username) references User(Username));""")
+    
+    cur.execute("""CREATE TABLE IF NOT EXISTS Submission_History (
+            ID INT PRIMARY KEY NOT NULL,
+            Username text NOT NULL,
+            Name text NOT NULL,
+            Type text NOT NULL,
+            Month_File text NOT NULL,
+            Year_File INT NOT NULL,
+            Date_Submitted INT NOT NULL,
+            FOREIGN KEY (Username) references User(Username));""")
+    
+    cur.execute("""CREATE TABLE IF NOT EXISTS Request (
+            ID INT PRIMARY KEY NOT NULL,
+            Username text NOT NULL,
+            Admin_Username text,
+            Description text NOT NULL,
+            Status text NOT NULL,
+            Date_Submitted INT NOT NULL,
+            Date_Accepted INT,
+            FOREIGN KEY (Username) references User(Username),
+            FOREIGN KEY (Admin_Username) references User(Username));""")
+    
+    conn.close()
---
+++ src/main/main.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+from tkinter import *
+import console
+import create_user_db
+
+def run():
+    # Create database for user accounts
+    create_user_db.create_tables()
+    
+    # Run console
+    app = console.TeqApp()
+    app.geometry('400x300')
+    app.title("TEQLIP Application")
+    app.mainloop()
+
+if __name__ == "__main__":
+    run()
