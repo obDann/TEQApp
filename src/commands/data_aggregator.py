7f87880a15de4733c07ec4633944ad3479088f33
Veracities <philip200156@hotmail.com>
2018-11-03 22:19:14 -0400

    Completed the automation of fixing mismatch data types in the values

--- src/commands/data_aggregator.py
+++ src/commands/data_aggregator.py
@@ -1,51 +1,52 @@
 import pandas as pd
 import numpy as np
+import re
 from command import Command
 from uploading_command import UploadingCommand
 
+
 class DataAggregator(UploadingCommand):
 
-   
     def __init__(self, template_name):
         '''
         (DataAggregator, str) -> None
-        
+
         Initializes a data aggregator, and injects a specific template
         name to the uploading command
-        '''        
+        '''
         UploadingCommand.__init__(self, template_name)
 
     def execute(self, df):
         '''
-        (Command, DataFrame) -> Abstract
-        
+        (Command, DataFrame) -> DataFrame
+
         Executes the command, assuming a dataframe object gets passed in.
+        Currently only checks for if the data matches the type the column is
+        suppose to be.
         '''
-        df = pd.DataFrame(df)
-        
+
         # Gets the column names to a list
-        column_name = df.columns.T.tolist()       
-        
-        # Gets the data in each column to a list
-        column_values = df.values.T.tolist()
-        print(column_name)
-        print(column_values)
-        
+        column_name = df.columns.T.tolist()
+
         for column in column_name:
-            # Gets the type that the column is suppose to be
-            # O = object (string), f = float, i = int
-            type = df[column].dtype.kind    
-            df[column].replace(' ', 'Hi')
-     
+            # Gets the type that the column is suppose to be in
+            # object(string),float or int
+            col_type = df[column].dtype.kind
+            try:
+                # Make sure all the data are the same type as the column
+                df[column].get_values().astype(col_type)
+            except ValueError:
+                # If a string is entered into a set of numeric data
+                if (type == np.dtype(int) or type == np.dtype(float)):
+                    # The value is set to 0
+                    df[column] = df.replace({column: r'[a-zA-z]*'},
+                                            {column: 0}, regex=True)
+        return df
+
     def executed_properly(self):
         '''
         (Command) -> boolean
-        
+
         Returns a boolean to determine if this command was executed properly
         '''
         return self._exec_status
-    
-if __name__ == "__main__":
-    some_command = DataAggregator('template name')
-    df = {'float': [1.66, 20.91], 'int': [3, 4], 'string' : ["test", " "]}
-    some_command.execute(df)
