0367176e22d4c899b2b1fa4fb6a5800754e6cbd8
davidhjh <david.jh.huang@gmail.com>
2018-11-05 01:34:58 -0500

    Created beginning of send functions and replies a base email. Further information required to complete the full function.

--- src/emailer/account_creation_emailer.py
+++ src/emailer/account_creation_emailer.py
@@ -1,4 +1,12 @@
+import sendgrid
+import os
+from sendgrid.helpers.mail import *
 from emailer import Emailer
+
+# for sending emails through python
+import smtplib
+# importing email module
+from email.mime.text import MIMEText
 
 class AccountCreationEmailer(Emailer):
 
@@ -9,3 +17,29 @@ class AccountCreationEmailer(Emailer):
         Initialize AccountCreationEmailer
         '''
         self.email = email
+
+    def send(self, recipient):
+        '''
+        (Emailer, str) -> None
+        
+        Sends an email to the corresponding recipient about their account
+        creation
+        '''
+        sg = sendgrid.SendGridAPIClient('SG.ME6PqRvVQeqs71zjlyHzCQ.cTKpy39VLj\
+        kY52e8SMQTMyMyqfUiqdzHaZGsEPq1VTk')
+        from_email = Email("teqapp.noreply@gmail.com")
+        # check if email is in database?
+        to_email = Email(recipient)
+        subject = "Email Verification"
+        # allow user to verify email by clicking a link?
+        content = Content("text/plain", "This email is to notify you that \
+        your account has been successfully created")
+        mail = Mail(from_email, subject, to_email, content)
+        response = sg.client.mail.send.post(request_body=mail.get())        
+
+    def sent_properly(self):
+        '''
+        (Emailer) -> boolean
+
+        Returns a boolean to determine whether the email could be sent or not
+        '''
--- src/emailer/password_forgetting_emailer.py
+++ src/emailer/password_forgetting_emailer.py
@@ -1,3 +1,6 @@
+import sendgrid
+import os
+from sendgrid.helpers.mail import *
 from emailer import Emailer
 
 class PasswordForgettingEmailer(Emailer):
@@ -9,3 +12,28 @@ class PasswordForgettingEmailer(Emailer):
         Initialize PasswordForgettingEmailer
         '''
         self.email = email
+
+    def send(self, recipient):
+        '''
+        (Emailer, str) -> None
+        
+        Sends an email to the corresponding recipient about their current
+        password.
+        '''
+        sg = sendgrid.SendGridAPIClient('SG.ME6PqRvVQeqs71zjlyHzCQ.cTKpy39VLj\
+        kY52e8SMQTMyMyqfUiqdzHaZGsEPq1VTk')
+        from_email = Email("teqapp.noreply@gmail.com")
+        # check if email is in database?
+        to_email = Email(recipient)
+        subject = "Forgot your password"
+        # allow user to reset their password? or show in email ??
+        content = Content("text/plain", "Change your password")
+        mail = Mail(from_email, subject, to_email, content)
+        response = sg.client.mail.send.post(request_body=mail.get())        
+
+    def sent_properly(self):
+        '''
+        (Emailer) -> boolean
+
+        Returns a boolean to determine whether the email could be sent or not
+        '''
