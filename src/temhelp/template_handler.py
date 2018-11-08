1bf4a025dc48055aa11b69e6630668c8651b15cf
obDann <dj.sioson@mail.utoronto.ca>
2018-11-07 21:22:26 -0500

    Believed final version of the template; saw inefficiencies, but found fixes

--- src/temhelp/template_handler.py
+++ src/temhelp/template_handler.py
@@ -2,30 +2,28 @@ from abc import ABC, abstractmethod
 
 class TemplateHandler(ABC):
 
-    def __init__(self, template_name, column_name):
+    @abstractmethod
+    def __init__(self, template_name):
         '''
-        (TemplateHandler, str, str) -> None
+        (TemplateHandler, str) -> None
 
         Initializes a template handler and injects a template name
-        and a column name
 
         RAISES TemplateNotEnteredException if there is no metadata for the
         template
-
-        RAISES ColumnNotInTemplateException if the specified column name is
-        not in the template
         '''
-        self._template_name = template_name
-        self._column_name = column_name
 
     @abstractmethod
-    def handle_template(self):
+    def handle_template(self, col_name):
         '''
-        (TemplateHandler) -> tuple of (bool, str, str)
+        (TemplateHandler, str) -> tuple of (bool, str, str)
 
         Returns tuple of a boolean to determine if the injected column name is
         in the template, a string representing the appropriate regex, and a
         string representing an example of the column
+
+        RAISES ColumnNotInTemplateException if the specified column name is
+        not in the template
         '''
 
     @abstractmethod
@@ -45,22 +43,25 @@ class TemplateHandler(ABC):
         '''
 
     @abstractmethod
-    def is_dropdown_value_mandatory(self):
+    def is_dropdown_value_mandatory(self, col_name):
         '''
-        (TemplateHandler) -> bool
+        (TemplateHandler, str) -> bool
 
         Determines if the column name injected into the template handler
         has strict and limited drop down values
+
+        RAISES ColumnNotInTemplateException if the specified column name is
+        not in the template
         '''
 
     @abstractmethod
-    def get_dropdown_values(self):
+    def get_dropdown_values(self, col_name):
         '''
-        (TemplateHandler) -> [list of str]
+        (TemplateHandler, str) -> [list of str]
 
         Returns the list of mandatory dropdown values for the injected
         column name.
 
-        If the column name is not 'dropdown value mandatory', this method
-        returns an empty list.
+        RAISES ColumnNotInTemplateException if the specified column name is
+        not in the template
         '''
