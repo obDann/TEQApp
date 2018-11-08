431feb663d91a6cca99befccef29730225fd865e
Veracities <philip200156@hotmail.com>
2018-11-07 20:50:35 -0500

    Made a mistake in branching, this is the commit for adding a report branch

--- src/report/database_retriever.py
+++ src/report/database_retriever.py
@@ -1,7 +1,14 @@
 import sqlite3
 
 def number_of_users(start_date, end_date):
+    '''
+    (str,str) -> 
+    Given the time period (start and end date) return the number of
+    users that are stored into the database within this period.
+    '''
     conn = sqlite3.connect('client_data.db')
     cur = conn.cursor()
     
     cur.execute("SELECT COUNT(*),Unique_ID_Value FROM Client where")
+    
+    
