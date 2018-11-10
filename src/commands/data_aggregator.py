f748fd17d305b0730d8881fa4754c6a13c3db0ea
Veracities <philip200156@hotmail.com>
2018-11-10 12:47:04 -0500

    Refactored data aggregator and made test cases for it

--- src/commands/data_aggregator.py
+++ src/commands/data_aggregator.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import numpy as np
 import re
+import sys
 from uploading_command import UploadingCommand
-import datetime
-
+sys.path.append("../temhelp")
+from template_handler import TemplateHandler
+sys.path.append("../tests")
 
 class DataAggregator(UploadingCommand):
 
@@ -15,53 +17,28 @@ class DataAggregator(UploadingCommand):
         name to the uploading command
         '''
         UploadingCommand.__init__(self, template_name)
+        self._postal_code = 'Postal Code'
+        self._processing_detail = 'Processing Detail'
+        
 
     def execute(self, df, th):
         '''
-        (Command, DataFrame) -> DataFrame
+        (DataAggregator, DataFrame, TemplateHandler) -> DataFrame
 
         Executes the command, assuming a dataframe object gets passed in.
         Currently only checks for if the data matches the type the column is
         suppose to be.
         '''
-        now = datetime.datetime.now()
         #template = th.get_template(self._template_name)
-        # Gets the column names to a list
-        #header_name = template.get_header()
-        header_name = ['Processing Detail','col2','Postal Code']
-        # Gets the list of regex
-        #regex = template.get_regex()
-        regex = ['','','([A-Z]\d){3}']
-        #str(now.year)+'/'+str(now.month)+"/"+str(now.day) 
-        
         self._exec_status = False
+    
+        # get a list of fields that are not formatted correctly
+        wrong_format_fields = parse_all_columns(df, 'template')
         
-        for header in range(len(header_name)):
-            count = 0
-            column = df.get(header_name[header])
-            if (column is not None):
-                for row in range(len(df.index)):
-                    # getting the regex from the list
-                    p = re.compile(regex[count])
-                    # Checks each row for matching regex
-                    if (not p.Processingmatch(column[row])):
-                        # Automation fix for postal code
-                        if (header_name[header] == "Postal Code"):
-                            new_col = self.postal_code_checker(column)
-                            df.update(new_col)
-                        # Automation fix for processing detail
-                        elif (header_name[header] == "Processing Detail"):
-                            df.replace(df.loc(header, row),str(now.year)+'/'
-                                       +str(now.month)+"/"+str(now.day))
-                        else:
-                            # Open GUI for user to change
-                            print("Open GUI")
-            # Shift the regex list down by 1 each column is done
-            count = count + 1
         self._exec_status = True        
         return df
-
-    def postal_code_checker(self, df):
+    
+    def _postal_code_checker(self, df):
             '''
             (DataFrame) ->  DataFrame
             The DataFrame of 'Postal_Code' is given, then return a new
@@ -96,8 +73,43 @@ class DataAggregator(UploadingCommand):
         '''
         return self._exec_status
 
-if __name__ == "__main__":
-    test = DataAggregator("test")
-    df = {'Processing Detail': [1,2,3], 'col2' : [9,2,5], 'Postal Code': ['M1M1M1','Y8Y8Y8']}
-    df = pd.Dataframe(df)
-    test.execute(df,'template handler')
+def parse_all_columns(df, template):
+    '''
+        (DataFrame, TemplateHandler) -> List of
+        [Tuple of (String, int)]
+        
+        Given a DataFrame and TemplateHandler, returns a tuple of all the
+        fields that are not matching the regex/format.
+    '''
+    # List to store the tuples
+    misformated = list()
+    # Gets the column names to a list
+    header_name = template.get_header()
+    # Gets the list of regex
+    regex = template.get_regex()
+    # for looping through regex list
+    count = 0
+    
+    for col_i in range(len(header_name)):
+        column = df.get(header_name[col_i])
+        if (column is not None):
+            # Loop through all the fields
+            for row in range(len(df.index)):
+                # getting the regex from the list
+                p = re.compile(regex[count])
+                # Checks each field to the matching regex
+                if (p.match(str(df.iat[row, col_i])) is None):
+                    # Get a list of drop down values
+                    #dropdown = template.get_dropdown_values(column)
+                    dropdown = drop_down[col_i]
+                    if (len(dropdown) != 0):
+                        # If its not in the drop down list
+                        if (column[row] not in dropdown):
+                            misformated.append((header_name[col_i], row))
+                    else:
+                        # Append to the misformated list
+                        misformated.append((header_name[col_i], row))    
+        # Shift the regex list down by 1 each column is done
+        count = count + 1
+            
+    return misformated
