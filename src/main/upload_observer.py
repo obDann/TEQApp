d22426975a9021d9edb957b30f1fff9c64f5e365
RiazCharania <riaz123456789@hotmail.com>
2018-11-11 20:16:58 -0500

    upadted upload observer

--- src/main/upload_observer.py
+++ src/main/upload_observer.py
@@ -29,4 +29,15 @@ class UploadObserver(Observer):
         my_fsf = fsf.FileSystemFetcher(temp_root)
         the_dataframe = my_fsf.execute()
         temp_root.mainloop()
-        print(the_dataframe)
+
+        # check if the file system fetcher executed properly
+        if (my_fsf.executed_properly()):
+            # we are going to be appropriately setting the headers. The
+            # headers are typically on the third row. The title is in the
+            # 1st row, so the headers are actually on the 3rd row.
+            header = the_dataframe.iloc[1]
+            # cut the rest of the dataframe
+            the_dataframe = the_dataframe[2:]
+            # and then rename the headers
+            the_dataframe = the_dataframe.rename(columns = header)
+            print(the_dataframe)
