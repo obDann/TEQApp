1e2f7babbb3a82c9572b95da03545e86adf42665
Veracities <philip200156@hotmail.com>
2018-11-21 20:08:44 -0500

    Connected custom query to the main app

--- src/main/custom_query_page.py
+++ src/main/custom_query_page.py
@@ -2,35 +2,9 @@ import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 import random
-#import teq_page as tp
-from custom_query_observer import *
-from button_observable import *
-
-
-class My_GUI(tk.Tk):
-
-    def __init__(self, *args, **kwargs):
-        
-        tk.Tk.__init__(self, *args, **kwargs)
-        self.cont = tk.Frame(self)
-
-        self.cont.pack(side="top", fill="both", expand = True)
-
-        self.cont.grid_rowconfigure(0, weight=1)
-        self.cont.grid_columnconfigure(0, weight=1)
-
-        self.display(CustomQueryPage)
-    
-    def display(self, page):
-        frame = page(self.cont, self)
-        frame.grid(row=0, column=0, sticky="nsew")
-        frame.tkraise()
-            
-    def set_page(self, f, name):
-        frame = f(self.cont, self, name)
-        frame.grid(row=0, column=0, sticky="nsew")
-        frame.tkraise()
-
+import csv
+import sqlite3
+import teq_page as tp
 
 class CustomQueryPage(tk.Frame):
     def __init__(self, parent, controller):
@@ -38,8 +12,6 @@ class CustomQueryPage(tk.Frame):
         tk.Frame.__init__(self,parent)
         label1 = tk.Label(self, text="Enter Your Command Here:")
         label1.pack(side=TOP,anchor=NW, padx=10)
-        
-        cq_obs = CustomQueryObserver()
         
         # The text box at the bottom
         self.text1 = tk.Text(self,width=80, height=1)
@@ -49,17 +21,13 @@ class CustomQueryPage(tk.Frame):
         self.text2 = tk.Text(self,width=10, height=1)
         self.text2.pack(side=TOP,fill=X, padx=10)        
         
-        obs_button1 = ButtonObservable()
-        obs_button1.set_button(tk.Button(self, text="Save",
-                            #command=lambda: obs_button1.raise_event(self)))
-                            command=lambda: self.export_to_csv()))
-                            
-        obs_button1.add_observer(cq_obs)
-        save = obs_button1.button        
-        save.pack(side=TOP,fill=X, padx= 5, pady= 5)
+        b1 = tk.Button(self, text="Save",
+                       command=lambda: self.export_to_csv())
+                                
+        b1.pack(side=TOP,fill=X, padx= 5, pady= 5)
     
-        back = tk.Button(self, text="Back")
-                         #command=lambda: controller.display(tp.TEQPage))
+        back = tk.Button(self, text="Back",
+                         command=lambda: controller.display(tp.TEQPage))
         back.pack(side=TOP,fill=X, padx = 5, pady=5) 
     
     def export_to_csv(self):
@@ -83,7 +51,7 @@ class CustomQueryPage(tk.Frame):
                 writer = csv.writer(csv_file, delimiter=',')
                 for line in result:
                     writer.writerow(line)                
-        except TypeError as e:
+        except sqlite3.Error as e:
             print("Invalid SQL input, with the message: " + e)
         # Finish message
         print("Finished executing query.")
@@ -102,8 +70,3 @@ class CustomQueryPage(tk.Frame):
         '''
         input_value = self.text2.get("1.0","end-1c")
         return input_value    
-        
-        
-if __name__ == "__main__":
-    app = My_GUI()
-    app.mainloop()
