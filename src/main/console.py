f91b54cdbdf1dbbc21a7fb7be0250df55a11a177
obDann <dj.sioson@mail.utoronto.ca>
2018-11-22 21:08:22 -0500

    Checkpoint; made a method so that Philip can run his page

--- src/main/console.py
+++ src/main/console.py
@@ -15,23 +15,49 @@ from file_upload_page import *
 class TeqApp(tk.Tk):
 
     def __init__(self, *args, **kwargs):
-        
+        '''
+        (TeqApp, *args, **kwargs) -> None
+
+        Initializes the TEQ application
+        '''
+        # make the root
         tk.Tk.__init__(self, *args, **kwargs)
         self.cont = tk.Frame(self)
-
         self.cont.pack(side="top", fill="both", expand = True)
 
         self.cont.grid_rowconfigure(0, weight=1)
         self.cont.grid_columnconfigure(0, weight=1)
-
+        # go to the main page on initialization
         self.display(MainPage)
 
     def display(self, page):
+        '''
+        (TeqApp, Frame) -> None
+
+        Goes to another page without providing anything
+        '''
         frame = page(self.cont, self)
         frame.grid(row=0, column=0, sticky="nsew")
         frame.tkraise()
-        
+
     def set_page(self, f, name):
+        '''
+        (TeqApp, Frame, str) -> None
+
+        Goes to another page while providing a specific name
+        '''
         frame = f(self.cont, self, name)
         frame.grid(row=0, column=0, sticky="nsew")
         frame.tkraise()
+
+    def insert_page_details(self, page, df, template_name):
+        '''
+        (TeqApp, Frame, DataFrame, str) -> None
+
+        Goes to another page while providing a dataframe and a template name
+        '''
+        my_frame = page(self.cont, self, df, template_name)
+        # set the grid
+        my_frame.grid(row=0, column=0, sticky="nsew")
+        # go to page
+        my_frame.tkraise()
